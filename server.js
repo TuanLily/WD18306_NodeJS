69412309895b377b1e39a868d840725837fde94e
TuanLily <122657799+TuanLily@users.noreply.github.com>
2024-03-08 22:44:47 +0700

    Hoàn thành tạo switch case nối các trang con
    Hoàn thành tạo switch case nối các trang con

--- server.js
+++ server.js
@@ -19,12 +19,13 @@ app.use('/', require('./src/client/routes/route.js'))
 // Đặt thư mục views
 app.set('views', path.join(__dirname, 'src', 'client', 'views'));
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+// app.get('/', (req, res) => {
+//     res.render('index');
+// });
 
 
 const port = process.env.PORT || 3000; // Sử dụng cổng mặc định là 3000 nếu không có biến môi trường PORT
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
